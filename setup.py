ae781cf04e698870b811c05bdfaea9a5c74bb1c6
tmtabor <tabor@broadinstitute.org>
2015-05-21 14:24:53 -0400

    Fixing repo for PyPi submission

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='gp',
+    name='genepattern-python',
     py_modules=['gp'],
     version='1.0.3',
     description='Library for programmatically interacting with GenePattern from Python.',
     author='Thorin Tabor',
     author_email='tabor@broadinstitute.org',
     url='https://github.com/genepattern/genepattern-python',
-    download_url='https://github.com/genepattern/genepattern-python/archive/master.zip',
+    download_url='https://github.com/genepattern/genepattern-python/archive/0.1.tar.gz',
     keywords=['genepattern', 'genomics', 'bioinformatics'],
-    classifiers=['Framework :: IPython']
+    classifiers=['Framework :: IPython'],
 )
