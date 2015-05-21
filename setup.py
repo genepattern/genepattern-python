dea1509398ffb833aee6579ec9231d8560cb5de7
tmtabor <tabor@broadinstitute.org>
2015-05-21 13:27:03 -0400

    Initial checkin

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='gp',
+    py_modules=['gp'],
+    version='1.0.3',
+    description='Library for programmatically interacting with GenePattern from Python.',
+    author='Thorin Tabor',
+    author_email='tabor@broadinstitute.org',
+    url='https://github.com/genepattern/genepattern-python',
+    download_url='https://github.com/genepattern/genepattern-python/archive/master.zip',
+    keywords=['genepattern', 'genomics', 'bioinformatics'],
+    classifiers=['Framework :: IPython']
+)
