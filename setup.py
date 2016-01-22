0d2c9117104c86e5b3643def9a04e9547a834b9b
tmtabor <tabor@broadinstitute.org>
2016-01-22 10:47:45 -0500

    Updated to 1.0.7

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='genepattern-python',
     py_modules=['gp'],
-    version='1.0.6',
+    version='1.0.7',
     description='Library for programmatically interacting with GenePattern from Python.',
     author='Thorin Tabor',
     author_email='tabor@broadinstitute.org',
     url='https://github.com/genepattern/genepattern-python',
-    download_url='https://github.com/genepattern/genepattern-python/archive/1.0.6.tar.gz',
+    download_url='https://github.com/genepattern/genepattern-python/archive/1.0.7.tar.gz',
     keywords=['genepattern', 'genomics', 'bioinformatics'],
 )
