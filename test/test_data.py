485a82835bc4e47af323f6327e1446b861768625
tmtabor <tabor@broadinstitute.org>
2017-11-17 12:24:14 -0800

    Travis CI setup

---
+++ test/test_data.py
@@ -0,0 +1,43 @@
+"""
+Tests for loading GCT and ODF files into pandas dataframes
+"""
+
+
+def test_gct_load_gpfile():
+    assert True
+
+
+def test_gct_load_file():
+    assert True
+
+
+def test_gct_load_url():
+    assert True
+
+
+def test_gct_load_path():
+    assert True
+
+
+def test_gct_load_string():
+    assert True
+
+
+def test_odf_load_gpfile():
+    assert True
+
+
+def test_odf_load_file():
+    assert True
+
+
+def test_odf_load_url():
+    assert True
+
+
+def test_odf_load_path():
+    assert True
+
+
+def test_odf_load_string():
+    assert True
