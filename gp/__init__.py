2464c143b85c2e36224caab99ac45b63c724c82c
tmtabor <tabor@broadinstitute.org>
2018-06-18 16:38:13 -0700

    Roll back namespace change

--- gp/__init__.py
+++ gp/__init__.py
@@ -1,7 +1,4 @@
-__authors__ = ['Thorin Tabor', 'Chet Birger']
-__copyright__ = 'Copyright 2014-2018, Regents of the University of California & Broad Institute'
-__version__ = '1.4.1'
-__status__ = 'Production'
+import warnings
 
 """
 GenePattern Python Client
@@ -9,4 +6,24 @@ GenePattern Python Client
 Compatible with Python 3.4+
 """
 
+__authors__ = ['Thorin Tabor', 'Chet Birger']
+__copyright__ = 'Copyright 2014-2018, Regents of the University of California & Broad Institute'
+__version__ = '1.5.0.RC4'
+__status__ = 'Production'
+
+# Import core functionality
 from .core import GPException, GPFile, GPJob, GPJobSpec, GPResource, GPServer, GPTask, GPTaskParam, GPJSONEncoder
+
+# Import subpackages, if available
+try:
+    import gp.utils
+except Exception:
+    # Ignore if subpackages are unavailable
+    True
+
+# # Warn if this is imported using the old `gp` namespace
+# if __name__ == 'gp':
+#     message = 'The GenePattern Python library has been restructured and now uses the `genepattern.client` package name. ' + \
+#               'Please import using that name as `gp` will be deprecated in the future'
+#     warnings.warn(message, PendingDeprecationWarning)
+#     print('WARNING: ' + message)
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-import genepattern.client
 from distutils.core import setup
+
+
+# Read version and other metadata from file
+__version__ = '1.5.0.RC4'
 
 setup(
     name='genepattern-python',
-    package_dir={'gp': 'genepattern/client', 'genepattern.client': 'genepattern/client'},
-    packages=['gp', 'genepattern.client'],
-    version=genepattern.client.__version__,
+    packages=['gp'],
+    version=__version__,
     description='Library for programmatically interacting with GenePattern from Python.',
     author='Thorin Tabor',
     author_email='tmtabor@cloud.ucsd.edu',
     url='https://github.com/genepattern/genepattern-python',
-    download_url='https://github.com/genepattern/genepattern-python/archive/' + genepattern.client.__version__ + '.tar.gz',
+    download_url='https://github.com/genepattern/genepattern-python/archive/' + __version__ + '.tar.gz',
     keywords=['genepattern', 'genomics', 'bioinformatics'],
     license='BSD'
 )
