4e1215b5e0f4eeae6a80960c9ba0dd9c2f45f62f
tmtabor <tmtabor@cloud.ucsd.edu>
2020-07-27 10:43:55 -0700

    Updating version number

--- gp/__init__.py
+++ gp/__init__.py
@@ -3,12 +3,12 @@ import warnings
 """
 GenePattern Python Client
 
-Compatible with Python 3.4+
+Compatible with Python 3.6+
 """
 
 __authors__ = ['Thorin Tabor', 'Chet Birger']
 __copyright__ = 'Copyright 2014-2020, Regents of the University of California & Broad Institute'
-__version__ = '20.05'
+__version__ = '20.07'
 __status__ = 'Production'
 
 # Import core functionality
@@ -21,9 +21,3 @@ except Exception:
     # Ignore if subpackages are unavailable
     True
 
-# # Warn if this is imported using the old `gp` namespace
-# if __name__ == 'gp':
-#     message = 'The GenePattern Python library has been restructured and now uses the `genepattern.client` package name. ' + \
-#               'Please import using that name as `gp` will be deprecated in the future'
-#     warnings.warn(message, PendingDeprecationWarning)
-#     print('WARNING: ' + message)
