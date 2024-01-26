ec6109095732c628785ee2edfcd6aa46ab3baf8b
tmtabor <tmtabor@cloud.ucsd.edu>
2024-01-26 11:47:53 -0800

    Fixed bug where get_recent_jobs() would throw error if a job was initialized without input parameters

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 # Read version and other metadata from file
-__version__ = '23.09'
+__version__ = '24.01'
 
 with open('README.md') as f:
     long_description = f.read()
